be9881bc6092a1c58378df1902da0bcfb5ef4cd4
Ruaann <Ruaan.nortje@plennegy.com>
2021-04-08 09:44:02 +0200

    Activate Library

---
+++ app/src/main/java/com/plennegy/plennegylibrary/AppUser.java
@@ -0,0 +1,14 @@
+package com.plennegy.plennegylibrary;
+
+public class AppUser
+{
+    String userName;
+    String userEmail;
+    String userPassword;
+    String userRole;
+    String userCell;
+    String userApplication;
+
+
+
+}//End of AppUser
